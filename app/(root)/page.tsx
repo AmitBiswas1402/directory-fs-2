4765754f64d34e27245e6f9f3b7f86a3fc368ebc
AmitBiswas1402 <amit.142biswas@gamil.com>
2024-11-04 21:18:59 +0530

    navbar and auth made

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      Home
+      <section className="pink_container">
+      <h1 className="heading">Pitch your startup ideas!!!</h1>
+
+      <p className="sub-heading !max-w-3xl">
+        Submit ideas, vote pitches & get noticed by investors.
+      </p>
+      </section>
     </>
   );
 }
